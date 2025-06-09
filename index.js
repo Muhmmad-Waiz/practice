d10f90dab039fd76d1c3d4f3b4d915bbf620d350
M.Waiz <waiz35202@gmail.com>
2025-06-10 03:37:10 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+// let num = 10;
+// console.log(num);
+// type Person = {
+//     name: string;
+// };
+function example(value) {
+    if (typeof value === "string") {
+        console.log(value.toUpperCase());
+    }
+    else {
+        console.log(value.toFixed(2));
+    }
+}
+example("waiz");
+example(40);
+//instance of
+var dog = /** @class */ (function () {
+    function dog() {
+    }
+    dog.prototype.bark = function () {
+        console.log("wow wow wow");
+    };
+    return dog;
+}());
+var cat = /** @class */ (function () {
+    function cat() {
+    }
+    cat.prototype.bark = function () {
+        console.log("meow meow");
+    };
+    return cat;
+}());
+function animalsound(animal) {
+    if (animal instanceof dog) {
+        animal.bark();
+    }
+    else {
+        animal.bark();
+    }
+}
+var dog1 = new dog();
+var cat1 = new cat();
+console.log(dog1);
+console.log(cat1);
+animalsound(dog1);
+animalsound(cat1);
