06d8f35980002753d53bc7041c75e8a7e5149efb
M.Waiz <waiz35202@gmail.com>
2025-06-11 04:58:47 +0500

    Added two number

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int sum = num1 + num2;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
