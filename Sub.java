3312e094d51838a9c0c6526dcf9061f1a2241d16
Muhammad-Waiz <waiz35202@gmail.com>
2025-06-11 05:12:22 +0500

    Added Diff files

---
+++ Sub.java
@@ -0,0 +1,10 @@
+public class Sub {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int Diff = num1 - num2;
+
+        System.out.println("The Diff is: " - Diff);
+    }
+}
